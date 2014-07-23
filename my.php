8c08b2afbf5896ce5e6ce10e366a39cb6511b720
bihe0832 <bihe0832@foxmail.com>
2014-07-23 22:20:34 +0800

    更新代码

--- my.php
+++ my.php
@@ -7,7 +7,6 @@
  */
 require_once (dirname(__FILE__).'/conf/config.php');
 ?>
-<html>
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, maximum-scale=1.0, user-scalable=1" />
